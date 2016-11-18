b014f11f9ad0699a8eebbd55307ae517199c91b4
Rodrigo Otavio van den Berg Maia <rodrigo@devlandia.net>
2016-11-18 20:34:49 +0000

    Initial commit

---
+++ samples/insert.sql
@@ -0,0 +1,74 @@
+INSERT INTO roydashes_stash(
+  id,
+  title,
+  dur,
+  num,
+  vol,
+  ex,
+  aId,
+  aNm,
+  tbId,
+  tbNm,
+  img,
+  als,
+  prev,
+  st,
+  str,
+  right,
+  md,
+  lab,
+  p,
+  fUd,
+  ma,
+  rnk,
+  tasort,
+  rtId,
+  rrId
+)
+VALUES (
+  108,
+  'PréludeDeLaSuiteNº1EnSolMajeurPourVioloncelle',
+  184.0,
+  1,
+  1,
+  false,
+  8,
+  'DanielMille',
+  106,
+  'LeFunambule',
+  '/VAN/Cover_Art/master/Orchard/829410758178.jpg',
+  'DanielMille',
+  '/Orchard/Thumb_Preview/Fulltrack/Orchard/126091/_829410758178/829410758178_1_1.mp3',
+  {
+    {
+      id:         58,
+      ctry:       'US',
+      en:         '2100.01.01',
+      st:         '2007.09.25',
+      stdt:       {
+        date: '2007-09-25T04:00:00.000Z'
+      },
+      defaultSt:  true
+    }
+  },
+  true,
+  true,
+  '/content/music/gen/Orchard/Thumb_Content/Full_AAC/Orchard/126091/_829410758178/829410758178_1_1.m4a',
+  {
+    {
+      id:   5,
+      ctry: 'US',
+      nm:   'Saravah',
+      p:    'TheOrchard'
+    }
+  },
+  2,
+  {
+    date: '2016-10-28T18:47:00.472Z'
+  },
+  false,
+  0.0,
+  54,
+  'Tra.72214917',
+  'Alb.72214916'
+);
